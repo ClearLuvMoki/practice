6a0521d5f60eb671189b21605aafd1f3da80f467
moki <2893096286@qq.com>
2021-04-29 16:21:02 +0800

    add:添加了下拉添加数据&&需要更改一些下拉获取数据过多渲染的问题&&需要更改以及下拉不流畅的问题

--- Trao-practice/oldZhihuMiniProgram/src/pages/index/index.jsx
+++ Trao-practice/oldZhihuMiniProgram/src/pages/index/index.jsx
@@ -5,6 +5,7 @@
  * @description 主页
  */
 import { Component } from 'react'
+import Taro from '@tarojs/taro'
 import { View, ScrollView, Image, Input } from '@tarojs/components'
 import { getNewsApi } from '../../utils/servers/homeApi/index' // 接口
 import NewsCard from '../../components/NewsCard/index'
@@ -16,12 +17,29 @@ class Index extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newsList: []
+      newsList: [],
+      screenHeight: '0px'
     }
   }
+  
 
   componentDidMount = () => {
     this.initList()
+    this.getScreenHeight()
+  }
+
+  /**
+   * @author ClearLuvMoki
+   * @filename index.jsx
+   * @date 2021-04-29 星期四
+   * @todo 获取屏幕高度
+   */
+  getScreenHeight = () => {
+    Taro.getSystemInfo().then(
+      (res) => {
+        this.setState({screenHeight: `${res?.screenHeight}px` })
+      }
+    )
   }
 
   /**
@@ -31,16 +49,36 @@ class Index extends Component {
    * @todo 获取数据列表
    */
   initList = () => {
-    getNewsApi().then(
+    getNewsApi(10, 0).then(
       (res) => {
         this.setState({newsList: res?.data})
       })
   }
 
+  /**
+   * @author ClearLuvMoki
+   * @filename index.jsx
+   * @date 2021-04-29 星期四
+   * @todo 滚到到页面底部获取数据
+   */
+   getDataByOnScrollToLower = (event) => {
+     console.log(event, 'event')
+     const { newsList } = this.state
+      getNewsApi(10, newsList.length).then(
+        (res) => {
+          this.setState({newsList: this.state.newsList.concat(res?.data)})
+      })
+   }
+
+
   render () {
-    const { newsList } = this.state
+    const { newsList, screenHeight } = this.state
+    const style = {
+      height: screenHeight
+    }
     const scrollTop = 0
-    const Threshold = 20
+    const Threshold = 5
+    
     return (
       <View>
         <View className='search flex-wrp'>
@@ -57,11 +95,13 @@ class Index extends Component {
         <ScrollView
           scrollY
           scrollWithAnimation
+          style={style}
           scrollTop={scrollTop} // 竖向滚动条位置
           upperThreshold={Threshold}  // 具体顶部/左侧多少距离触发事件
           lowerThreshold={Threshold}  // 具体底部/右侧多少距离触发事件
           onScrollToUpper={() => {}} // 滚动到顶部/左侧触发事件
-          onScrollToLower={() => {}} // 滚动到底部/右侧触发事件
+          onScrollToLower={this.getDataByOnScrollToLower} // 滚动到底部/右侧触发事件
+          onScroll={this.onScroll}
         >
           {
             newsList && newsList.length > 0 && newsList.map(item => {
--- Trao-practice/oldZhihuMiniProgram/src/utils/servers/homeApi/index.js
+++ Trao-practice/oldZhihuMiniProgram/src/utils/servers/homeApi/index.js
@@ -11,13 +11,19 @@ import Taro from '@tarojs/taro'
  * @filename index.js
  * @date 2021-04-28 星期三
  * @todo 获取知乎列表
+ * @param { limit } 条数
+ * @param { offset } 开始位置
  */
-const getNewsApi = async () => {
+const getNewsApi = async (limit, offset) => {
     Taro.showLoading({title: '加载中'})
     let resData = {}
     await Taro.request({
         url: 'https://api.zhihu.com/explore/answers', // 知乎接口
         method: 'GET',
+        data: {
+            limit,
+            offset
+        }
     }).then(
         (res) => {
             Taro.hideLoading()
